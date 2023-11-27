d881e851bf65ea0af4a05d903f38d05fb55164d9
Aaron Le <116464393+adl409@users.noreply.github.com>
2023-11-27 14:01:00 -0600

    Labeling Each Body Segment Added

--- body-segmentation.py
+++ body-segmentation.py
@@ -5,11 +5,11 @@ import mediapipe as mp
 mp_holistic = mp.solutions.holistic
 holistic = mp_holistic.Holistic()
 
-# Open webcam
+# Open camera
 cap = cv2.VideoCapture(0)
 
 while cap.isOpened():
-    # Read a frame from the webcam
+    # Read a frame from the camera
     ret, frame = cap.read()
     if not ret:
         break
@@ -29,6 +29,49 @@ while cap.isOpened():
         mp_drawing.draw_landmarks(
             frame, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS
         )
+        body_segment_labels = {
+            (0, 0): "Nose",
+            (4, 8): "Eyes",
+            (1, 3): "Eyes",
+            (9, 10): "Mouth",
+            (11, 12): "Chest",
+            (11, 13): "Bicep",
+            (12, 14): "Bicep",
+            (13, 15): "Forearm",
+            (14, 16): "Forearm",
+            (18, 20): "Palms",
+            (15, 17): "Palms",
+            (12, 23): "Torso",
+            (24, 28): "Legs",
+            (23, 27): "Legs"
+
+
+        }
+        # Add labels to each limb
+        for connection, label in body_segment_labels.items():
+            start_point, end_point = connection
+        
+        # Testing for plotting points
+        # for connection in mp_holistic.POSE_CONNECTIONS:
+        #     start_point = connection[0]
+        #     end_point = connection[1]
+
+            start_landmark = results.pose_landmarks.landmark[start_point]
+            end_landmark = results.pose_landmarks.landmark[end_point]
+
+            h, w, _ = frame.shape
+            start_x, start_y = int(start_landmark.x * w), int(start_landmark.y * h)
+            end_x, end_y = int(end_landmark.x * w), int(end_landmark.y * h)
+
+            # Calculate the midpoint for labeling
+            mid_x, mid_y = (start_x + end_x) // 2, (start_y + end_y) // 2
+
+            # Shows points Testing
+            # cv2.putText(frame, f"{start_point}-{end_point}", (mid_x, mid_y),
+            #             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
+            
+            cv2.putText(frame, label, (mid_x, mid_y),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
 
     # Display the frame
     cv2.imshow("Real-time Body Segmentation", frame)
